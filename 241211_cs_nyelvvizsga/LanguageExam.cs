cd02c286d48ad46aff3c3b3154678e146a1ecea2
Petrik Diak <diak@petrik.hu>
2024-12-11 11:54:15 +0100

    Add project files.

---
+++ 241211_cs_nyelvvizsga/LanguageExam.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//this is a poorly named class
+//each instance has statistics of language exams of that language over multiple years
+
+namespace _241211_cs_nyelvvizsga
+{
+	internal class LanguageExam
+	{
+		private string languageName;
+		private int startYear;
+		private List<int> numberOfSuccessfuls;
+		private List<int> numberOfFailed;
+
+		public LanguageExam(string languageName, int startYear, List<int> numberOfSuccessfuls, List<int> numberOfFailed)
+		{
+			if (numberOfFailed.Count() != numberOfSuccessfuls.Count())
+			{
+				throw new ArgumentException("the number of years is different between the successful and unsuccessful lists \n (LanguageExam class constructor)");
+			}
+			else
+			{
+				this.languageName = languageName;
+				this.startYear = startYear;
+				this.numberOfSuccessfuls = numberOfSuccessfuls;
+				this.numberOfFailed = numberOfFailed;
+			}
+		}
+
+		public string LanguageName { get => languageName; }
+		public int StartYear { get => startYear; }
+		public List<int> NumberOfSuccessfuls { get => numberOfSuccessfuls; }
+		public List<int> NumberOfFailed { get => numberOfFailed; }
+
+		public int AllExams
+		{
+			get => (numberOfSuccessfuls.Sum() + NumberOfFailed.Sum());
+		}
+
+		public int HowManySuccessfulsInYear(int year)
+		{
+			if (year < startYear || year < startYear + numberOfSuccessfuls.Count())
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+			else
+			{
+				return numberOfSuccessfuls[year - startYear];
+			}
+
+		}
+		public int HowManyFailedInYear(int year)
+		{
+			if (year < startYear || year < startYear + numberOfSuccessfuls.Count())
+			{
+				throw new ArgumentOutOfRangeException();
+			}
+			else
+			{
+				return numberOfFailed[year - startYear];
+			}
+		}
+
+
+
+
+	}
+}
---
+++ 241211_cs_nyelvvizsga/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _241211_cs_nyelvvizsga
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string successfulFilePath = "sikeres.csv";
+			string failedFilePath = "sikertelen.csv";
+			List<LanguageExam> list = new List<LanguageExam>();
+			StreamReader rSuccessful = new StreamReader(successfulFilePath, new UTF7Encoding());
+			StreamReader rFailed = new StreamReader(failedFilePath, new UTF7Encoding());
+			int startYear;
+			startYear = int.Parse(rSuccessful.ReadLine().Split(';')[1]);
+			Console.WriteLine(startYear);
+			rFailed.ReadLine();
+			while (!rFailed.EndOfStream && !rSuccessful.EndOfStream)
+			{
+				List<string> successInputList = rSuccessful.ReadLine().Split(';').ToList<string>();
+				List<string> failedInputList = rFailed.ReadLine().Split(';').ToList<string>();
+				if (successInputList.Count != failedInputList.Count ||
+					successInputList[0] != failedInputList[0])
+				{
+					throw new Exception($"{successfulFilePath} and {failedFilePath} seem to belong to different databases");
+				}
+				else
+				{
+					List<int> numberOfSuccessfuls = new List<int>();
+					List<int> numberOfFailed = new List<int>();
+
+					for (int i = 1; i < successInputList.Count; i++)
+					{
+						numberOfSuccessfuls.Add(int.Parse(successInputList[i]));
+						numberOfFailed.Add(int.Parse(failedInputList[i]));
+						//unhandled potential error: I'm expecting the numbers to be numbers in the source files
+					}
+					Console.WriteLine(successInputList[0]);
+					list.Add(new LanguageExam(successInputList[0], startYear, numberOfSuccessfuls, numberOfFailed));
+				}
+				//not handled potentional error: it will do something stupid if the two input files have
+				//a different amount of lines
+			}
+
+			Console.WriteLine("2. feladat: 3 legnépszerűbb nyelv");
+			list.Sort(comparison: (x, y) => y.AllExams - x.AllExams);
+			for (int i = 0; i < 3; i++)
+			{
+				Console.WriteLine(list[i].LanguageName);
+            }
+
+		}
+	}
+}
+
